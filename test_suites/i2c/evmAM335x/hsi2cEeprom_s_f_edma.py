7e56f6e72c326c5c9c53639b44bd44e1038f6193
Yebio Mesfin <ymesfin@ti.com>
2013-01-24 05:02:16 -0500

    The patch contains scripts to automate Starterware examples for i2c read from eeprom using EDMA and interrupt mode. There are six Starterware examples, two for each platform (beaglebone, evmAM335x and evmskAM335x).

---
+++ test_suites/i2c/evmAM335x/hsi2cEeprom_s_f_edma.py
@@ -0,0 +1,26 @@
+# The script executes hsi2cEeprom_edma test application.
+# The script enters user input interactivley and verify the test result automatically.
+
+#list of all requirments  
+requires = ['evmAM335x','i2c','uart','edma']
+test_status = ""
+#Hardcoded in the test application 
+number_of_bytes_to_read = 50
+#load the boot file
+runstp_import('test_suites/templates/load_template.py')
+#load test application binary file located on starterware binary directory.  
+if (serial_load('hsi2c_eeprom_edma/Release/hsi2cEeprom_edma.bin') == 1):
+    #test data is detected or verified by expect   
+    index = platform.expect('([A-Z0-9][A-Z0-9],){' + str(number_of_bytes_to_read -1) + '}')
+    if index == 0:
+        test_status = "P"
+        print "TEST PASS"
+    else: 
+        test_status = "F"
+        print "TEST FAIL"
+else:
+    test_status = "F"
+    print "hsi2cEeprom_edma.bin loading failed.\n"
+# Return test Result. Valid RC: 'p' => passed, 'f' => failed, 'e' => error
+# Perf is an optional hash with performance data {'name': "testname", 'values': [], 'units': "values unit"}
+testresult = {'RC': test_status, 'Comments': "EePROM READ SUCCESS", 'Perf': None}
